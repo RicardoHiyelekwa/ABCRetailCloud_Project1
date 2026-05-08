116164726913b00b81ccd6e423ae001a2764c85b
ricardo.hiyelekwa <ricardo.hiyelekwa@minfin.gov.ao>
2026-05-08 07:24:21 +0100

    ajustes e integraçaõ com azure function

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
-
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ABCRetailCloud.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddSingleton<TableStorageService>();
 builder.Services.AddSingleton<BlobStorageService>();
-builder.Services.AddSingleton<QueueStorageService>();
 builder.Services.AddSingleton<FileStorageService>();
+builder.Services.AddSingleton<QueueStorageService>();
+builder.Services.AddSingleton<TableStorageService>();
 
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
 
 app.MapControllerRoute(
-name:"default",
-pattern:"{controller=Home}/{action=Index}/{id?}"
-);
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
